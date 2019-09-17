ed97bccd9898efa0458d894e9d04efb1334a6405
Marquis <theusual@icloud.com>
2019-09-17 09:21:32 -0600

    first commitmommit

---
+++ api/api.py
@@ -0,0 +1,23 @@
+import flask
+from flask import request
+from redirect_functions import test_redirects
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
+@app.route('/api/v1/redirect-test', methods=['GET'])
+def api_filter():
+    params = request.args
+
+    base64_csv = params['csv']
+    test_url_index = params['testurl']
+    target_url_index = params['targeturl']
+
+    return test_redirects(base64_csv, test_url_index, target_url_index)
+
+app.run()
---
+++ api/redirect_functions.py
@@ -0,0 +1,56 @@
+import csv
+import ssl
+import base64
+from time import sleep
+from flask import jsonify
+from urllib.parse import urlparse, unquote
+from urllib.request import urlopen, Request, HTTPError, URLError
+
+# SSL bypass
+ssl._create_default_https_context = ssl._create_unverified_context
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
+
+# {base64_csv, test_url_index target_url_index}
+# base64_csv takes the url encoded base64 csv string
+# test_url_index takes the test urls column number
+# target_url_index takes the target urls column number
+def test_redirects(base64_csv, test_url_index, target_url_index):
+    csv_string = unquote(base64_csv)
+    test_url_index = int(test_url_index)
+    target_url_index = int(target_url_index)
+
+    output = []
+    base64_parsed = base64.b64decode(csv_string).decode('unicode_escape').splitlines()
+    csv_reader = csv.reader(base64_parsed, delimiter=',')
+
+    for row in csv_reader:
+        if len(row) > 0:
+            if len(urlparse(row[test_url_index]).scheme) > 0:
+                # swap url
+                url = row[test_url_index]
+
+                # send request
+                try:
+                    req = Request(url=url, headers=headers)
+                    open = urlopen(req)
+                    status = open.getcode()
+                    final = open.geturl()
+
+                    # check if end url matches the target
+                    if final.split('.com')[1] == row[target_url_index].split('.com')[1]:
+                        correct_redirect = True;
+                    else:
+                        correct_redirect = False;
+
+                # catch 404s and whatever else
+                except HTTPError as e:
+                    output.append({'url': url, 'status': e.code, 'matches': correct_redirect})
+                    continue
+                except URLError as e:
+                    output.append({'url': url, 'status': e.code, 'matches': correct_redirect})
+                    continue
+
+                # output
+                output.append({'url': url, 'status': status, 'matches': correct_redirect})
+
+    return jsonify(output)
