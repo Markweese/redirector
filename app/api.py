3d27601048361c833288d459db130877062ffac5
Marquis <theusual@icloud.com>
2019-09-17 20:49:29 -0600

    update templating structure and transfer syncronous operations to javascript

--- app/api.py
+++ app/api.py
@@ -1,6 +1,6 @@
 import flask
 from flask import request, render_template
-from .redirect_functions import test_redirects
+from redirect_functions import test_redirect, parse_urls
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -8,15 +8,16 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-    return render_template("dashboard.html")
+    return render_template("form.html")
 
 @app.route('/api/v1/redirect-test', methods=['POST'])
 def api_filter():
-    base64_csv = request.files['file']
+    csv_file = request.files['file']
     test_url_index = request.form['testurl']
     target_url_index = request.form['targeturl']
+    parsed_urls = parse_urls(csv_file, test_url_index, target_url_index)
 
-    return test_redirects(base64_csv, test_url_index, target_url_index)
+    return render_template("dashboard.html", urls=parsed_urls)
 
 if __name__ == '__main__':
     app.run()
--- app/redirect_functions.py
+++ app/redirect_functions.py
@@ -1,6 +1,6 @@
 import csv
 import ssl
-import base64
+import json
 from time import sleep
 from flask import jsonify
 from urllib.parse import urlparse, unquote
@@ -11,10 +11,10 @@ ssl._create_default_https_context = ssl._create_unverified_context
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
 
 # {csv_file, test_url_index target_url_index}
-# csv_file takes the url encoded base64 csv string
+# csv_file takes the csv file
 # test_url_index takes the test urls column number
 # target_url_index takes the target urls column number
-def test_redirects(csv_file, test_url_index, target_url_index):
+def parse_urls(csv_file, test_url_index, target_url_index):
     csv_string = csv_file.stream.read().decode("utf-8").splitlines()
     test_url_index = int(test_url_index)
     target_url_index = int(target_url_index)
@@ -22,35 +22,46 @@ def test_redirects(csv_file, test_url_index, target_url_index):
     output = []
     csv_reader = csv.reader(csv_string, delimiter=',')
 
-    #
     for row in csv_reader:
         if len(row) > 0:
             if len(urlparse(row[test_url_index]).scheme) > 0:
                 # swap url
-                url = row[test_url_index]
+                output.append({'testurl':row[test_url_index], 'targeturl':row[target_url_index]})
 
-                # send request
-                try:
-                    req = Request(url=url, headers=headers)
-                    open = urlopen(req)
-                    status = open.getcode()
-                    final = open.geturl()
+    return json.dumps(output)
 
-                    # check if end url matches the target
-                    if final.split('.com')[1] == row[target_url_index].split('.com')[1]:
-                        correct_redirect = True;
-                    else:
-                        correct_redirect = False;
+# {test_url target_url}
+# test_url takes the test url
+# target_url takes the target url
+def test_redirect(test_url, target_url):
+    output = {}
 
-                # catch 404s and whatever else
-                except HTTPError as e:
-                    output.append({'url': url, 'status': e.code, 'matches': correct_redirect})
-                    continue
-                except URLError as e:
-                    output.append({'url': url, 'status': e.code, 'matches': correct_redirect})
-                    continue
+    if len(urlparse(test_url).scheme) > 0:
+        # swap url
+        url = test_url
 
-                # output
-                output.append({'url': url, 'status': status, 'matches': correct_redirect})
+        # send request
+        try:
+            req = Request(url=url, headers=headers)
+            open = urlopen(req)
+            status = open.getcode()
+            final = open.geturl()
+
+            # check if end url matches the target
+            if final.split('.com')[1] == target_url.split('.com')[1]:
+                correct_redirect = True;
+            else:
+                correct_redirect = False;
+
+        # catch 404s and whatever else
+        except HTTPError as e:
+            output['url'] = url
+            output['status'] = e.code
+            output['matches'] = correct_redirect
+
+        except URLError as e:
+            output['url'] = url
+            output['status'] = e.code
+            output['matches'] = correct_redirect
 
     return jsonify(output)
